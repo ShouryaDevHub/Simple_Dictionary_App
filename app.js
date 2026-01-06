f1a7a2efd9af96121ed29a85263d8deba4226eb1
Shourya Rawat <rawatshourya07@gmail.com>
2026-01-06 16:20:35 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const btn = document.querySelector("button");
+const input = document.querySelector("input");
+const result = document.querySelector("#result");
+
+btn.addEventListener("click", async () => {
+  let word = document.querySelector("input").value;
+
+  if (word === "") {
+    alert("please enter some word");
+    return;
+  }
+
+  try {
+    let res = await fetch(
+      `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`
+    );
+    let data = await res.json(); // data pharse.
+
+    if (data.title) {
+      result.innerText = data.message;
+      return;
+    }
+
+    let meaning = data[0].meanings[0].definitions[0].definition;
+
+    result.innerHTML = `<b>Meaning :-</b> ${meaning}`;
+  } catch (e) {
+    result.innerText = "Error fetching data";
+  }
+});
